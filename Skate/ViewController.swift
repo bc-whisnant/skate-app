1557c2b44b59bd948970e012756b648ca1f4941c
bc-whisnant <bc.whisnant@gmail.com>
2018-10-09 19:49:31 -0400

    Initial Commit

---
+++ Skate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skate
+//
+//  Created by Galvatron on 10/9/18.
+//  Copyright © 2018 Galvatron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
